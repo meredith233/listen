1614525ba4427b31be3ff1bbe7c320ceaa1215c7
liangjuhong <850871021@qq.com>
2020-03-31 17:12:11 +0800

    add activity

---
+++ app/src/main/java/com/example/listen/activity/PlayingActivity.java
@@ -0,0 +1,16 @@
+package com.example.listen.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.listen.R;
+
+public class PlayingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playing);
+    }
+}
