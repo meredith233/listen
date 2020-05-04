981f92c7d31647d872ab6b30f55d4a8d3acae0e8
liangjuhong <850871021@qq.com>
2020-05-04 21:56:00 +0800

    backup

--- app/src/main/java/com/example/listen/activity/playing/PlayingActivity.java
+++ app/src/main/java/com/example/listen/activity/playing/PlayingActivity.java
@@ -301,7 +301,7 @@ public class PlayingActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            finish();
+            this.finish();
         }
         return super.onOptionsItemSelected(item);
     }
